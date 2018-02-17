0e97b3f911bcb163837771e8d75b6126021c9cb8
Krabat Tilt <krabat.tilt@gmx.de>
2018-02-17 20:57:36 +0100

    - Added ConverterBase class to enable creation of MarkupExtension ValueConverter. - Removed obsolete Interactivity dll as NuGet version is already used

---
+++ FlatMVVM/Utils/ConverterBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace FlatMVVM.Utils
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Use ValueConverters as markup extension.
+    /// </summary>
+    public abstract class ConverterBase : MarkupExtension
+    {
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+    }
+}
