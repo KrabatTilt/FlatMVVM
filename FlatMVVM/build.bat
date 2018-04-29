f783895c6a2701c19617e0a341551a415ae405fa
Krabat Tilt <krabat.tilt@gmx.de>
2018-04-29 16:17:03 +0200

    - removed old lib folder - fixed issues in build.bat and nuspec file

--- FlatMVVM/build.bat
+++ FlatMVVM/build.bat
@@ -17,7 +17,9 @@ if "%nuget%" == "" (
 
 REM Build
 
-"%programfiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe" FlatMVVM.sln /t:FlatMVVM /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%programfiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe" ../FlatMVVM.sln /t:FlatMVVM /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+Rem "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" ../FlatMVVM.sln /t:FlatMVVM /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
 
 
 REM Package
@@ -26,5 +28,5 @@ mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net45
 
-call %nuget% pack "FlatMVVM\FlatMVVM.nuspec" -symbols -o Build -Version %version% -p Configuration=%config%
+call %nuget% pack "FlatMVVM.nuspec" -verbosity detailed -symbols -o Build -Version %version% -p Configuration=%config%
 REM %nuget% pack "FlatMVVM.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
