cdc89c41f564f742987822a0cd484c7fd92e7b60
KrabatTilt <krabat.tilt@gmx.de>
2020-06-04 00:20:50 +0200

    - Added BooleanParameter type as helper for static bool value usage in xaml

---
+++ FlatMVVM/Utils/BooleanParameter.cs
@@ -0,0 +1,21 @@
+﻿namespace TT.FlatMVVM.Utils
+{
+
+    /// <summary>
+    /// Defines a type that can be used in xaml to define static bool values.
+    /// </summary>
+    public static class BooleanParameter
+    {
+
+        /// <summary>
+        /// Represents a true value
+        /// </summary>
+        public static bool True => true;
+
+        /// <summary>
+        /// Represents a false value
+        /// </summary>
+        public static bool False => false;
+
+    }
+}
